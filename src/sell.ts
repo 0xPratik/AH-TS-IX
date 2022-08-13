36253042e517d77f39cd716e2088ad9f97172202
ProPratik1405 <sariapratik@gmail.com>
2022-08-13 15:25:33 +0545

    Sell Added

--- src/sell.ts
+++ src/sell.ts
@@ -8,35 +8,91 @@ import {
 import {
   getAtaForMint,
   getAuctionHouseProgramAsSigner,
-  getAuctionHouseTradeState
+  getAuctionHouseTradeState,
+  AuctionHouse,
+  getMetadata,
+  getPriceWithMantissa
 } from './utils';
+import { getMint } from '@solana/spl-token';
 
 export const createSell = async (connection: anchor.web3.Connection) => {
   try {
-    // Accounts/Publickeys
     const mint = new anchor.web3.PublicKey('');
-    const auctionHousePubKey = new anchor.web3.PublicKey('');
-    const feeAccountPubkey = new anchor.web3.PublicKey('');
     const metadataPubkey = new anchor.web3.PublicKey('');
     const freeSellerTradeState = new anchor.web3.PublicKey('');
 
     const tokenAccountPubkey = (await getAtaForMint(mint, wallet.publicKey))[0];
+    const TreasuryMintData = await getMint(
+      connection,
+      new anchor.web3.PublicKey(AuctionHouse.mint)
+    );
+    const NFTMintData = await getMint(connection, mint);
 
     const [programAsSigner, programAsSignerBump] =
       await getAuctionHouseProgramAsSigner();
 
+    const buyPriceAdjusted = new anchor.BN(
+      await getPriceWithMantissa(1, TreasuryMintData.decimals)
+    );
+
+    const tokenSizeAdjusted = new anchor.BN(
+      await getPriceWithMantissa(1, NFTMintData.decimals)
+    );
+
+    const [SellertradeState, SellertradeBump] = await getAuctionHouseTradeState(
+      new anchor.web3.PublicKey(AuctionHouse.address),
+      wallet.publicKey,
+      tokenAccountPubkey,
+      new anchor.web3.PublicKey(AuctionHouse.mint),
+      mint,
+      tokenSizeAdjusted,
+      buyPriceAdjusted
+    );
+
+    const [freeTradeState, freeTradeBump] = await getAuctionHouseTradeState(
+      new anchor.web3.PublicKey(AuctionHouse.address),
+      wallet.publicKey,
+      tokenAccountPubkey,
+      new anchor.web3.PublicKey(AuctionHouse.mint),
+      mint,
+      tokenSizeAdjusted,
+      new anchor.BN(0)
+    );
+
     const accounts: SellInstructionAccounts = {
-      auctionHouse: auctionHousePubKey,
-      auctionHouseFeeAccount: feeAccountPubkey,
-      authority: wallet.publicKey,
-      metadata: metadataPubkey,
+      auctionHouse: new anchor.web3.PublicKey(AuctionHouse.address),
+      auctionHouseFeeAccount: new anchor.web3.PublicKey(
+        AuctionHouse.feeAccount
+      ),
+      authority: new anchor.web3.PublicKey(AuctionHouse.authority),
+      metadata: await getMetadata(mint),
       wallet: wallet.publicKey,
-      freeSellerTradeState: freeSellerTradeState,
       programAsSigner: programAsSigner,
+      freeSellerTradeState: freeTradeState,
       tokenAccount: tokenAccountPubkey,
-      sellerTradeState: programAsSigner
+      sellerTradeState: SellertradeState
     };
-    // const args: SellInstructionArgs = {};
+    const args: SellInstructionArgs = {
+      freeTradeStateBump: freeTradeBump,
+      programAsSignerBump: programAsSignerBump,
+      tradeStateBump: SellertradeBump,
+      buyerPrice: buyPriceAdjusted,
+      tokenSize: tokenSizeAdjusted
+    };
+
+    const instruction = createSellInstruction(accounts, args);
+    const { blockhash } = await connection.getLatestBlockhash();
+
+    const transaction = new anchor.web3.Transaction({
+      recentBlockhash: blockhash
+    });
+
+    transaction.add(instruction);
+    wallet.signTransaction(transaction);
+    const rawTx = transaction.serialize();
+    const sig = await connection.sendRawTransaction(rawTx);
+    await connection.confirmTransaction(sig, 'confirmed');
+    return sig;
   } catch (error) {
     console.log('SELL ERROR', error);
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,29 @@
 import * as anchor from '@project-serum/anchor';
 import { PROGRAM_ID } from '@metaplex-foundation/mpl-auction-house';
+import { TOKEN_METADATA_PROGRAM_ID } from './constants';
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   TOKEN_PROGRAM_ID
 } from '@solana/spl-token';
 
 export const AUCTION_HOUSE = 'auction_house';
+
+export const AuctionHouse = {
+  address: 'Gr31akcY9wb7Gsnu4ej39MydBfxcT8mehZtKxFTsCAai',
+  mint: 'So11111111111111111111111111111111111111112',
+  authority: 'Eqp8mRhQpjk2nDYtZJjkyshJBc5EZudPDcMNhaXGuyJN',
+  creator: 'Eqp8mRhQpjk2nDYtZJjkyshJBc5EZudPDcMNhaXGuyJN',
+  feeAccount: 'AnUxX7EmyRZegQtv4xYaBBWMaYG5uoLdJdMHfWXu4CLo',
+  treasuryAccount: 'FxaGuPmq9qNDX1qWAMrGUfwGp3cqahW1T5eViPDG7VR2',
+  withdrawAccount: 'Eqp8mRhQpjk2nDYtZJjkyshJBc5EZudPDcMNhaXGuyJN',
+  treasuryWithdrawAccount: 'Eqp8mRhQpjk2nDYtZJjkyshJBc5EZudPDcMNhaXGuyJN',
+  ahBump: 255,
+  ahFeeBump: 254,
+  ahTreasuryBump: 255,
+  sellerFeeBasisPoints: 500,
+  canChangeSalePrice: false,
+  requiresSignOff: false
+};
 
 export async function getAuctionHouseProgramAsSigner(): Promise<
   [anchor.web3.PublicKey, number]
@@ -58,3 +76,18 @@ export async function getPriceWithMantissa(
 
   return Math.ceil(price * mantissa);
 }
+
+export const getMetadata = async (
+  mint: anchor.web3.PublicKey
+): Promise<anchor.web3.PublicKey> => {
+  return (
+    await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from('metadata'),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mint.toBuffer()
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    )
+  )[0];
+};
